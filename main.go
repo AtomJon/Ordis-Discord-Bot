c27aa5bde7a9f207b5a304191dc745fb55541976
AtomJon <jonbruskin@gmail,com>
2020-11-27 17:10:19 +0100

    Initial upload of work

---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"encoding/gob"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+const (
+
+	//Token : Discord bot authentication token
+	Token = "NzgxNTU4NDE4NzU0MjQwNTUy.X7_Y_w.ZbeE7bNWbp8CqOBOOWFDsrlYhP8"
+
+	//DataFile : Filename of data file
+	DataFile = "message_count.dat"
+)
+
+func main() {
+
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(messageCreate)
+
+	// In this example, we only care about receiving message events.
+	dg.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsGuildMessages)
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the authenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	// Ignore messages by bots
+	if m.Author.Bot {
+		return
+	}
+
+	data := map[string]int{}
+
+	inputFile, err := os.Open(DataFile)
+	if err != nil {
+		panic(err)
+	}
+
+	gob.NewDecoder(inputFile).Decode(&data)
+
+	user, exists := data[m.Author.ID]
+	if !exists {
+		// Is the user already in database. Else, set messages to one
+		user = 1
+	} else {
+		// If the user is in database, increment messages by one
+		user++
+	}
+
+	data[m.Author.ID] = user
+
+	fmt.Printf("%s has written %d messages\n", m.Author.Username, user)
+
+	outputFile, err := os.Create(DataFile)
+	if err != nil {
+		panic(err)
+	}
+
+	gob.NewEncoder(outputFile).Encode(&data)
+}
