76ea73a3964d53dec04aac812756add22cbe749d
AtomJon <jonbruskin@gmail,com>
2020-12-21 10:31:57 +0100

    User join handling

--- main.go
+++ main.go
@@ -65,6 +65,11 @@ func main() {
 // message is created on any channel that the authenticated bot has access to.
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
+	if m.Type == discordgo.MessageTypeGuildMemberJoin {
+		fmt.Println("User joined")
+		return
+	}
+
 	// Ignore all messages created by the bot itself
 	// This isn't required in this specific example but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
